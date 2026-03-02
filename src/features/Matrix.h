d4dff10de7bbc9a9389aa41b61d4c6513b13caf4
Elumenix <89927445+Elumenix@users.noreply.github.com>
2026-03-02 18:09:14 +0000

    Refactored matrix class to be much cleaner, correct, and fit c++ coding standards

--- src/features/Matrix.h
+++ src/features/Matrix.h
@@ -2,145 +2,57 @@
 #include <cstddef>
 #include "Tuple.h"
 
-template <std::size_t width, std::size_t height> struct Matrix {
-    public:
-    inline static const Matrix<4,4> Identity = {
-        1,0,0,0,
-        0,1,0,0,
-        0,0,1,0,
-        0,0,0,1
-    };
-    
-    // For access to the underlying matrix for performance sensitive contexts.
-    // I want it to be private by default so that the bracket operators get used instead
-    float* data() { return mat; }
-    const float* data() const { return mat; }
-
+template <std::size_t Width, std::size_t Height> 
+class Matrix {
+public:
+    // Constructors
     Matrix() = default;
     Matrix(std::initializer_list<float> values) {
-        if (values.size() != width * height) {
+        if (values.size() != Width * Height) {
             throw std::invalid_argument("Wrong number of values");
         }
 
-        std::size_t i = 0;
+        int i = 0;
         for (float v : values) {
-            mat[i++] = v;
+            m_data[i++] = v;
         } 
     }
 
-    template <size_t n>
-    Matrix(const float (&arr)[n]) {
-        for (size_t i = 0; i < n; i++) {
-            mat[i] = arr[i];
-        }
-    }
+    // Raw data access
+    float* data() { return m_data; }
+    const float* data() const { return m_data; }
 
-    // Apparently a indexing on a pointer does pointer arithmatic on it and dereferences it, so this is actually how you index a matrix
-    // I did not know that this was how that worked in c++ until I tried to implement this
-    float* operator[](std::size_t col) {
-        return &mat[col * height];
-    }
+    // Member access
+    float* operator[](std::size_t col) { return &m_data[col * Height]; }
+    const float* operator[](std::size_t col) const { return &m_data[col * Height]; }
 
-    const float* operator[](std::size_t col) const {
-        return &mat[col * height];
-    }
+    // Arithmatic
+    template <std::size_t OtherWidth>
+    Matrix<OtherWidth,Height> operator*(const Matrix<OtherWidth, Width>& other) const;
+    Matrix<Width, Height> operator*(float scalar) const;
+    Matrix<Width, Height>& operator*=(float scalar);
 
-    // If the 3 dimension values don't line up, the IDE will flag it in the editor
-    template <std::size_t other_width>
-    Matrix<other_width,height> operator*(const Matrix<other_width, width>& other) const {
-        Matrix<other_width, height> ret = Matrix<other_width, height>();
+    // Comparison
+    bool operator==(const Matrix<Width, Height>& other) const;
+    bool operator!=(const Matrix<Width, Height>& other) const { return !(*this == other); }
 
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < other_width; j++) {
-                for (int k = 0; k < height; k++) {
-                    ret[i][j] += (*this)[i][k] * other[k][j];
-                }
-            }
-        }
-
-        return ret;
-    }
-
-    bool operator==(const Matrix<width, height>& other) const {
-        const float* mat2 = other.data();
-
-        for (std::size_t i = 0; i < width * height; i++)
-            if (std::abs(mat[i] - mat2[i]) > 1e-3f)
-                return false;
-
-        return true;
-    }
-
-    bool operator!=(const Matrix<width, height>& other) const {
-        return !(*this == other);
-    }
-
-    Matrix<height, width> Transpose() const {
-        Matrix ret = Matrix<height, width>();
-        
-        for (int x = 0; x < height; x++) {
-            for (int y = 0; y < width; y++) {
-                ret[x][y] = (*this)[y][x];
-            }
-        }
-
-        return ret;
-    }
-
-    float Determinant() const {
-        static_assert(width == height, "Determinant only exists for square matrices");
-
-        if constexpr (width == 2) {
-            // ad - bc
-            return mat[0]*mat[3] - mat[1]*mat[2];
-        }
-        else {
-            float determinant = 0;
-
-            for (int i = 0; i < width; i++) {
-                determinant += (*this)[0][i] * Cofactor(0, i);
-            }
-
-            return determinant;
-        }
-    }
-
-    Matrix<width-1, height-1> SubMatrix(int rowRemoved, int colRemoved) const;
-    float Minor(int row, int col) const;
-    float Cofactor(int row, int col) const;
-    constexpr bool IsInvertible() const {
-        float det = Determinant();
-
-        // If the determinant is essentially 0, this matrix can't be inverted
-        return abs(det) > 1e-3;
-    }
-
-    Matrix<width, height> Inverse() const {
-        static_assert(width == height, "Only square matrices can be inverted");
-
-        bool isInvertible = IsInvertible();
-        assert(isInvertible && "This matrix cannot be inverted");
-
-        Matrix m2 = Matrix<width, height>();
-        float det = Determinant();
-
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                m2[x][y] = Cofactor(y, x) / det;
-            }
-        }
-
-        return m2;
-    }
+    // Matrix Operations
+    Matrix<Height, Width> transpose() const;
+    Matrix<Width, Height> inverse() const;
+    Matrix<Width-1, Height-1> subMatrix(int rowRemoved, int colRemoved) const;
+    float determinant() const; 
+    float minor(int row, int col) const;
+    float cofactor(int row, int col) const;
+    bool  isInvertible() const { return std::abs(determinant()) > 1e-3f; }
 
 
-    // Lets the matrix print to screen
-    friend std::ostream& operator<<(std::ostream& os, const Matrix<width, height>& m) {
+    // Stream Output
+    friend std::ostream& operator<<(std::ostream& os, const Matrix<Width, Height>& m) {
         os << "\n";
 
-        for (std::size_t i = 0; i < width * height; i++) {
-            os << m.mat[i] << " ";
-            if (i % width == width - 1) {
+        for (int i = 0; i < Width * Height; i++) {
+            os << m.m_data[i] << " ";
+            if (i % Width == Width - 1) {
                 os << "\n";
             }
             else {
@@ -150,71 +62,178 @@ template <std::size_t width, std::size_t height> struct Matrix {
         return os;
     }
 
-    private:
-    float mat[width * height] = {};
+private:
+    float m_data[Width * Height] = {};
 };
 
 
-// Tuples can only multiply by 4x4 Matrices because they ar a 1x4 Matrix
-Tuple operator*(const Matrix<4, 4>& mat, const Tuple& other) {
-    float arr[4] = {other.x, other.y, other.z, other.w};
-    float ret[4] = {}; 
+// =============================================================================
+// Template definitions
+// =============================================================================
 
-    for (int i = 0; i < 4; i++) {
-        float sum = 0;
-        for (int j = 0; j < 4; j++) {
-            sum += mat[i][j] * arr[j];
+template <std::size_t Width, std::size_t Height>
+template <std::size_t OtherWidth>
+inline Matrix<OtherWidth, Height> Matrix<Width, Height>::operator*(const Matrix<OtherWidth, Width> &other) const
+{
+    Matrix<OtherWidth, Height> ret = Matrix<OtherWidth, Height>();
+
+    for (int row = 0; row < Width; row++) {
+        for (int col = 0; col < OtherWidth; col++) {
+            for (int k = 0; k < Height; k++) {
+                ret[row][col] += (*this)[row][k] * other[k][col];
+            }
         }
-        ret[i] = sum;
     }
 
-    return Tuple(ret[0], ret[1], ret[2], ret[3]);
+    return ret;
 }
 
-template <std::size_t width, std::size_t height>
-Matrix<width, height> operator*(const Matrix<width, height>& mat, float scalar) {
-    Matrix<width, height> result = mat;
-    float* d = result.data();
+template <std::size_t Width, std::size_t Height>
+inline Matrix<Width, Height> Matrix<Width, Height>::operator*(float scalar) const {
+    Matrix<Width, Height> result = *this;
 
-    for (std::size_t i = 0; i < width * height; i++)
-        d[i] *= scalar;
+    for (int i = 0; i < Width * Height; ++i) {
+        result.m_data[i] *= scalar;
+    }
+
     return result;
 }
 
-template <std::size_t width, std::size_t height>
-Matrix<width, height> operator*(float scalar, const Matrix<width, height>& mat) {
+template <std::size_t Width, std::size_t Height>
+inline Matrix<Width, Height>& Matrix<Width, Height>::operator*=(float scalar) {
+    for (int i = 0; i < Width * Height; ++i) {
+        m_data[i] *= scalar;
+    }
+
+    return *this;
+}
+
+template <std::size_t Width, std::size_t Height>
+inline bool Matrix<Width, Height>::operator==(const Matrix<Width, Height> &other) const
+{
+
+    for (std::size_t i = 0; i < Width * Height; i++) {
+        if (std::abs(m_data[i] - other.m_data[i]) > 1e-3f) return false;
+    }
+
+    return true;
+}
+
+template <std::size_t Width, std::size_t Height>
+inline Matrix<Height, Width> Matrix<Width, Height>::transpose() const
+{
+    Matrix<Height, Width> ret;
+        
+        for (int col = 0; col < Width; col++) {
+            for (int row = 0; row < Height; row++) {
+                ret[row][col] = (*this)[col][row];
+            }
+        }
+
+        return ret;
+}
+
+template <std::size_t Width, std::size_t Height>
+inline Matrix<Width, Height> Matrix<Width, Height>::inverse() const
+{
+    static_assert(Width == Height, "Only square matrices can be inverted");
+    assert(isInvertible() && "Attempted to invert a non-invertible matrix");
+
+    Matrix m2 = Matrix<Width, Height>();
+    const float det = determinant();
+
+    for (int row = 0; row < Height; row++) {
+        for (int col = 0; col < Width; col++) {
+            m2[row][col] = cofactor(col, row) / det;
+        }
+    }
+
+    return m2;
+}
+
+template <size_t Width, size_t Height>
+inline Matrix<Width - 1, Height - 1> Matrix<Width, Height>::subMatrix(int rowRemoved, int colRemoved) const
+{
+    Matrix<Width-1,Height-1> result;
+    int rowIndex = 0;
+
+    for (int row = 0; row < Height; row++) {
+        if (row == rowRemoved) continue;
+        int colIndex = 0;
+
+        for (int col = 0; col < Width; col++) {
+            if (col == colRemoved) continue;
+            result[rowIndex][colIndex++] = (*this)[row][col];
+        } 
+        rowIndex++;
+    }
+
+    return result;
+}
+
+template <std::size_t Width, std::size_t Height>
+inline float Matrix<Width, Height>::determinant() const
+{
+    static_assert(Width == Height, "Determinant only exists for square matrices");
+
+    if constexpr (Width == 2) {
+        // ad - bc
+        return m_data[0]*m_data[3] - m_data[1]*m_data[2];
+    }
+    else {
+        float determinant = 0;
+
+        for (int col = 0; col < Width; col++) {
+            determinant += (*this)[0][col] * cofactor(0, col);
+        }
+
+        return determinant;
+    }
+}
+
+template <std::size_t Width, std::size_t Height>
+inline float Matrix<Width, Height>::minor(int row, int col) const {
+    Matrix<Width-1,Height-1> m = subMatrix(row, col);
+    return m.determinant();
+}
+
+template <std::size_t Width, std::size_t Height>
+inline float Matrix<Width, Height>::cofactor(int row, int col) const
+{
+    // even sum is positive, odd sum is negative
+    float sign = ((row + col) % 2) ? -1.0f : 1.0f;
+
+    return minor(row, col) * sign;
+}
+
+
+// =============================================================================
+// Non-member operators
+// =============================================================================
+
+// Scalar * Matrix (commutative counterpart to Matrix * scalar)
+template <std::size_t Width, std::size_t Height>
+inline Matrix<Width, Height> operator*(float scalar, const Matrix<Width, Height>& mat) {
     return mat * scalar;
 }
 
-template <size_t width, size_t height>
-inline Matrix<width-1, height-1> Matrix<width, height>::SubMatrix(int rowRemoved, int colRemoved) const {
-    float newArr[(width-1) * (height-1)] = {};
-    int index = 0;
-
-    for (int y = 0; y < height; y++) {
-        if (y == rowRemoved) continue;
-
-        for (int x = 0; x < width; x++) {
-            if (x == colRemoved) continue;
-            newArr[index] = (*this)[y][x];
-            index++;
-        } 
-    }
-
-    return Matrix<width-1, height-1>(newArr);
+// Tuples can only multiply by 4x4 Matrices because they are a 1x4 Matrix
+inline Tuple operator*(const Matrix<4, 4>& mat, const Tuple& t) {
+    return Tuple(
+        mat[0][0]*t.x + mat[0][1]*t.y + mat[0][2]*t.z + mat[0][3]*t.w,
+        mat[1][0]*t.x + mat[1][1]*t.y + mat[1][2]*t.z + mat[1][3]*t.w,
+        mat[2][0]*t.x + mat[2][1]*t.y + mat[2][2]*t.z + mat[2][3]*t.w,
+        mat[3][0]*t.x + mat[3][1]*t.y + mat[3][2]*t.z + mat[3][3]*t.w
+    );
 }
 
-template <std::size_t width, std::size_t height>
-inline float Matrix<width, height>::Minor(int row, int col) const {
-    Matrix<width-1,height-1> m = SubMatrix(row, col);
-    return m.Determinant();
-}
 
-template <std::size_t width, std::size_t height>
-inline float Matrix<width, height>::Cofactor(int row, int col) const
-{
-    // even sum is positive, odd sum is negative
-    int sign = ((row + col) % 2) * -2 + 1;
-
-    return Minor(row, col) * sign;
-}
+// =============================================================================
+// Constants
+// =============================================================================
+inline const Matrix<4,4> IdentityMatrix = {
+    1,0,0,0,
+    0,1,0,0,
+    0,0,1,0,
+    0,0,0,1
+};
--- tests/matrix_test.cpp
+++ tests/matrix_test.cpp
@@ -93,103 +93,103 @@ TEST(MatrixTest, TupleMultiplication) {
 
 TEST(MatrixTest, IdentityMatrix) {
     Matrix A = Matrix<4,4>({0,1,2,4,1,2,4,8,2,4,8,16,4,8,16,32});
-    EXPECT_EQ((A * Matrix<4,4>::Identity), A);
+    EXPECT_EQ((A * IdentityMatrix), A);
 
     Tuple b = Tuple(1,2,3,4);
-    EXPECT_EQ((Matrix<4,4>::Identity * b), b);
+    EXPECT_EQ((IdentityMatrix * b), b);
 }
 
 TEST(MatrixTest, TransposeTest) {
     Matrix A = Matrix<4,4>({0,9,3,0,9,8,0,8,1,8,5,3,0,0,5,8});
     Matrix T = Matrix<4,4>({0,9,1,0,9,8,8,0,3,0,5,5,0,8,3,8});
 
-    EXPECT_EQ(A.Transpose(), T);
+    EXPECT_EQ(A.transpose(), T);
 }
 
 TEST(MatrixTest, TransposeIdentity) {
-    Matrix A = Matrix<4,4>::Identity;
+    Matrix A = IdentityMatrix;
 
-    EXPECT_EQ(A.Transpose(), A);
+    EXPECT_EQ(A.transpose(), A);
 }
 
 TEST(MatrixTest, Determinant2x2) {
     Matrix A = Matrix<2,2>({1,5,-3,2});
 
-    EXPECT_FLOAT_EQ(A.Determinant(), 17.0f);
+    EXPECT_FLOAT_EQ(A.determinant(), 17.0f);
 }
 
 TEST(MatrixTest, SubMatrix32) {
     Matrix A = Matrix<3,3>({1,5,0,-3,2,7,0,6,-3});
     Matrix E = Matrix<2,2>({-3,2,0,6});
 
-    EXPECT_EQ(A.SubMatrix(0, 2), E);
+    EXPECT_EQ(A.subMatrix(0, 2), E);
 }
 
 TEST(MatrixTest, SubMatrix43) {
     Matrix A = Matrix<4,4>({-6,1,1,6,-8,5,8,6,-1,0,8,2,-7,1,-1,1});
     Matrix E = Matrix<3,3>({-6,1,6,-8,8,6,-7,-1,1});
 
-    EXPECT_EQ(A.SubMatrix(2, 1), E);
+    EXPECT_EQ(A.subMatrix(2, 1), E);
 }
 
 TEST(MatrixTest, TestMinor) {
     Matrix A = Matrix<3,3>({3,5,0,2,-1,-7,6,-1,5});
-    Matrix B = A.SubMatrix(1,0);
+    Matrix B = A.subMatrix(1,0);
 
-    EXPECT_FLOAT_EQ(B.Determinant(), 25);
-    EXPECT_FLOAT_EQ(A.Minor(1,0), 25);
+    EXPECT_FLOAT_EQ(B.determinant(), 25);
+    EXPECT_FLOAT_EQ(A.minor(1,0), 25);
 }
 
 TEST(MatrixTest, Cofactors) {
     Matrix A = Matrix<3,3>({3,5,0,2,-1,-7,6,-1,5});
 
-    EXPECT_FLOAT_EQ(A.Minor(0,0), -12);
-    EXPECT_FLOAT_EQ(A.Cofactor(0,0), -12);
-    EXPECT_FLOAT_EQ(A.Minor(1,0), 25);
-    EXPECT_FLOAT_EQ(A.Cofactor(1,0), -25);
+    EXPECT_FLOAT_EQ(A.minor(0,0), -12);
+    EXPECT_FLOAT_EQ(A.cofactor(0,0), -12);
+    EXPECT_FLOAT_EQ(A.minor(1,0), 25);
+    EXPECT_FLOAT_EQ(A.cofactor(1,0), -25);
 }
 
 TEST(MatrixTest, Determinant3x3) {
     Matrix A = Matrix<3,3>({1,2,6,-5,8,-4,2,6,4});
 
-    EXPECT_FLOAT_EQ(A.Cofactor(0,0), 56);
-    EXPECT_FLOAT_EQ(A.Cofactor(0,1), 12);
-    EXPECT_FLOAT_EQ(A.Cofactor(0,2), -46);
-    EXPECT_FLOAT_EQ(A.Determinant(), -196);
+    EXPECT_FLOAT_EQ(A.cofactor(0,0), 56);
+    EXPECT_FLOAT_EQ(A.cofactor(0,1), 12);
+    EXPECT_FLOAT_EQ(A.cofactor(0,2), -46);
+    EXPECT_FLOAT_EQ(A.determinant(), -196);
 }
 
 TEST(MatrixTest, Determinant4x4) {
     Matrix A = Matrix<4,4>({-2,-8,3,5,-3,1,7,3,1,2,-9,6,-6,7,7,-9});
 
-    EXPECT_FLOAT_EQ(A.Cofactor(0,0), 690);
-    EXPECT_FLOAT_EQ(A.Cofactor(0,1), 447);
-    EXPECT_FLOAT_EQ(A.Cofactor(0,2), 210);
-    EXPECT_FLOAT_EQ(A.Cofactor(0,3), 51);
-    EXPECT_FLOAT_EQ(A.Determinant(), -4071);
+    EXPECT_FLOAT_EQ(A.cofactor(0,0), 690);
+    EXPECT_FLOAT_EQ(A.cofactor(0,1), 447);
+    EXPECT_FLOAT_EQ(A.cofactor(0,2), 210);
+    EXPECT_FLOAT_EQ(A.cofactor(0,3), 51);
+    EXPECT_FLOAT_EQ(A.determinant(), -4071);
 }
 
 TEST(MatrixTest, CheckInvertible) {
     Matrix A = Matrix<4,4>({6,4,4,4,5,5,7,6,4,-9,3,-7,9,1,7,-6});
 
-    EXPECT_FLOAT_EQ(A.Determinant(), -2120);
-    EXPECT_EQ(A.IsInvertible(), true);
+    EXPECT_FLOAT_EQ(A.determinant(), -2120);
+    EXPECT_EQ(A.isInvertible(), true);
 }
 
 TEST(MatrixTest, CheckNotInvertible) {
     Matrix A = Matrix<4,4>({-4,2,-2,-3,9,6,2,6,0,-5,1,-5,0,0,0,0});
 
-    EXPECT_FLOAT_EQ(A.Determinant(), 0);
-    EXPECT_EQ(A.IsInvertible(), false);
+    EXPECT_FLOAT_EQ(A.determinant(), 0);
+    EXPECT_EQ(A.isInvertible(), false);
 }
 
 TEST(MatrixTest, InverseMatrix1) {
     Matrix A = Matrix<4,4>({-5,2,6,-8,1,-5,1,8,7,7,-6,-7,1,-3,7,4});
-    Matrix B = A.Inverse();
+    Matrix B = A.inverse();
 
-    EXPECT_FLOAT_EQ(A.Determinant(), 532);
-    EXPECT_FLOAT_EQ(A.Cofactor(2,3),  -160);
+    EXPECT_FLOAT_EQ(A.determinant(), 532);
+    EXPECT_FLOAT_EQ(A.cofactor(2,3),  -160);
     EXPECT_FLOAT_EQ(B[3][2], -160.0 / 532.0);
-    EXPECT_FLOAT_EQ(A.Cofactor(3,2), 105);
+    EXPECT_FLOAT_EQ(A.cofactor(3,2), 105);
     EXPECT_FLOAT_EQ(B[2][3], 105.0 / 532.0);
 
     Matrix C = Matrix<4,4>({0.21805, 0.45113, 0.24060, -0.04511, 
@@ -205,7 +205,7 @@ TEST(MatrixTest, InverseMatrix2) {
                             -0.07692, 0.12308, 0.02564, 0.03077,
                             0.35897, 0.35897, 0.43590, 0.92308,
                             -0.69231, -0.69231, -0.76923, -1.92308});
-    EXPECT_EQ(A.Inverse(), B);
+    EXPECT_EQ(A.inverse(), B);
 }
 
 TEST(MatrixTest, InverseMatrix3) {
@@ -214,7 +214,7 @@ TEST(MatrixTest, InverseMatrix3) {
                             -0.07778, 0.03333, 0.36667, -0.33333,
                             -0.02901, -0.14630, -0.10926, 0.12963,
                             0.17778, 0.06667, -0.26667, 0.33333});
-    EXPECT_EQ(A.Inverse(), B);
+    EXPECT_EQ(A.inverse(), B);
 }
 
 TEST(MatrixTest, InverseEquality) {
@@ -222,5 +222,5 @@ TEST(MatrixTest, InverseEquality) {
     Matrix B = Matrix<4,4>({8,2,2,2,3,-1,7,0,7,0,5,5,6,-2,0,5});
     Matrix C = A * B;
 
-    EXPECT_EQ(C * B.Inverse(), A);
+    EXPECT_EQ(C * B.inverse(), A);
 }
