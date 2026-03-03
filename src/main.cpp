2a22e68cab0a55cb903b6e40efd160700d34631f
Elumenix <89927445+Elumenix@users.noreply.github.com>
2026-03-03 16:40:50 +0000

    clock exercise works

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include "features/Tuple.h"
 #include "Environment.h"
 #include "Projectile.h"
+#include "features/Matrix.h"
+#include "features/Transformations.h"
 #include "features/Canvas.h"
 
 int main() {
@@ -10,18 +12,14 @@ int main() {
     int image_width = 100;
     int image_height = 100;
     Canvas canvas = Canvas(image_width, image_height);
-    Color red = Color(1,0,0);
+    Color white = Color(1,1,1);
 
-    // Testing out the projectile
-    Projectile p = Projectile(Point(0,1,0), Vector(1,1.8,0));    
-    Environment e = Environment(Vector(0, -0.05, 0), Vector(-0.01, 0, 0));
-
-    while (p.position.y >= 0 && p.position.y <= image_height && p.position.x <= image_width && p.position.x >= 0) {
-        //std::clog << "(" << p.position.x << ", " << p.position.y << ", " << p.position.z << ")" << std::endl;
-        canvas.WritePixelAt(p.position.x, image_height - p.position.y - 1, red);
-
-        p.position = p.position + p.velocity;
-        p.velocity = p.velocity + e.gravity + e.wind;
+    float clockRadius = image_width * .45f;
+    for (int i = 0; i < 12; i++) {
+        Tuple p = Point(0,0,0);
+        Matrix rot = transformations::rotationY(i * (M_PI / 6));
+        p = rot * Point(0,0,1) * clockRadius; 
+        canvas.WritePixelAt((int)roundf(p.x + image_width / 2), (int)roundf(p.z + image_height / 2), white);
     }
 
     std::string ppm = canvas.CanvasToPPM();
