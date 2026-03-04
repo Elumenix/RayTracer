8d4edcab8118bf5d885e9f6d8cae883eed1c89de
Elumenix <89927445+Elumenix@users.noreply.github.com>
2026-03-04 14:59:06 +0000

    basic ray logic finished

--- src/features/Ray.h
+++ src/features/Ray.h
@@ -1,5 +1,12 @@
 #pragma once
 #include "Tuple.h"
+#include "Sphere.h"
+
+struct IntersectionResult {
+    float t1;
+    float t2;
+    bool hit;
+};
 
 class Ray {
 public:
@@ -7,5 +14,31 @@ public:
     Tuple direction;
 
     Ray(Tuple p_origin, Tuple v_direction) : origin(p_origin), direction(v_direction){};
-    Tuple position(float t) {return origin + t * direction; };
+    Tuple position(float t) const {return origin + t * direction; };
+    IntersectionResult intersects(Sphere s) const {
+        IntersectionResult res;
+
+        // Sphere is currently hard-coded to be at the world origin
+        Tuple sphereToRay = origin - Point(0,0,0);
+        float a = DotProduct(direction, direction); // squared distance
+        float b = 2 * DotProduct(direction, sphereToRay);
+        float c = DotProduct(sphereToRay, sphereToRay) - 1;
+
+        float discriminant = b * b - 4 * a * c;
+        
+        // Does the ray completely miss the sphere
+        if (discriminant < 0) {
+            res.hit = false;
+            return res;
+        }
+        
+        float discSqrt = sqrtf(discriminant);
+
+        // Get two intersection points wether they are in front of or behind the sphere
+        res.t1 = (-b - discSqrt) / (2 * a);
+        res.t2 = (-b + discSqrt) / (2 * a);
+        res.hit = true;
+        
+        return res;
+    }
 };
--- tests/ray_test.cpp
+++ tests/ray_test.cpp
@@ -17,5 +17,52 @@ TEST(RayTest, PositionQuery) {
     EXPECT_EQ(r.position(1), Point(3,3,4));
     EXPECT_EQ(r.position(-1), Point(1,3,4));
     EXPECT_EQ(r.position(2.5), Point(4.5,3,4));
-    
+}
+
+TEST(RayTest, TwoPointIntersection) {
+    Ray r = Ray(Point(0,0,-5), Vector(0,0,1));
+    Sphere s;
+    IntersectionResult xs = r.intersects(s);
+
+    EXPECT_EQ(xs.hit, true);
+    EXPECT_FLOAT_EQ(xs.t1, 4.0f);
+    EXPECT_FLOAT_EQ(xs.t2, 6.0f);
+}
+
+TEST(RayTest, TangentIntersection) {
+    Ray r = Ray(Point(0,1,-5), Vector(0,0,1));
+    Sphere s;
+    IntersectionResult xs = r.intersects(s);
+
+    EXPECT_EQ(xs.hit, true);
+    EXPECT_FLOAT_EQ(xs.t1, 5.0f);
+    EXPECT_FLOAT_EQ(xs.t2, 5.0f);
+}
+
+TEST(RayTest, RayMissed) {
+    Ray r = Ray(Point(0,2,-5), Vector(0,0,1));
+    Sphere s;
+    IntersectionResult xs = r.intersects(s);
+
+    EXPECT_EQ(xs.hit, false);
+}
+
+TEST(RayTest, RayInsideSphere) {
+    Ray r = Ray(Point(0,0,0), Vector(0,0,1));
+    Sphere s;
+    IntersectionResult xs = r.intersects(s);
+
+    EXPECT_EQ(xs.hit, true);
+    EXPECT_FLOAT_EQ(xs.t1, -1.0f);
+    EXPECT_FLOAT_EQ(xs.t2, 1.0f);
+}
+
+TEST(RayTest, RayBehindSphere) {
+    Ray r = Ray(Point(0,0,5), Vector(0,0,1));
+    Sphere s;
+    IntersectionResult xs = r.intersects(s);
+
+    EXPECT_EQ(xs.hit, true);
+    EXPECT_FLOAT_EQ(xs.t1, -6.0f);
+    EXPECT_FLOAT_EQ(xs.t2, -4.0f);
 }
