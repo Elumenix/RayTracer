f96c383dab7d31fac327f8c8481412a98625e1ec
Elumenix <Elumenix@users.noreply.github.com>
2026-07-04 19:44:45 +0000

    deploy: 7ac1e17cf03ecbd658548aaec6cb51d9637da919

--- ray_worker.js
+++ ray_worker.js
@@ -4,50 +4,9 @@ let Module = null;
 let activeRenderId = 0;
 
 onmessage = async (e) => {
-    const { yaml, maxDepth, renderId } = e.data;
-    activeRenderId = renderId;
+    const message = e.data;
 
-    // Initialize WASM module once
-    if (!Module) {
-        Module = await initWasm();
-    }
-
-    // Cancel any previous render
-    Module.ccall("cancel_render");
-
-    // Calling the c++ render function
-    Module.ccall("render", null, ["string", "number"], [yaml, maxDepth]);
-
-    // Begin polling progress
-    pollProgress(renderId);
-};
-
-function pollProgress(renderId) {
-    // If a new render started, stop polling
-    if (renderId !== activeRenderId) return;
-
-    const progress = Module.ccall("get_progress", "number", [], []);
-    const total = Module.ccall("get_height", "number", [], []);
-
-    console.log(`Render progress: ${progress+1}/${total}`);
-
-    // Send progress update
-    postMessage({ type: "progress", progress: progress + 1, total });
-
-    if ((progress + 1) < total) {
-        // Continue polling
-        setTimeout(() => pollProgress(renderId), 50);
-    }
-    else {
-
-        /*
-        // Error handling - just outputting a message currently
-        const error = Module.ccall('get_last_error', 'string', [], [])
-        if (error !== "") {
-            console.log('error:', error)
-        }
-        */
-
+    if (message.type === "done") {
         // Retrieve information about the rendered image
         const width = Module.ccall('get_width', 'number', [], []);
         const height = Module.ccall('get_height', 'number', [], []);
@@ -56,13 +15,27 @@ function pollProgress(renderId) {
         Module.ccall('copy_to_buffer', null, ['number'], [bufferPtr]);
         const pixels = new Uint8ClampedArray(Module.HEAPU8.buffer, bufferPtr, bufferSize).slice();
 
+        // Send info back to the main thread
         postMessage({
-            type: "done",
+            type: "stream",
             width,
             height,
             pixels
         });
 
         Module.ccall("free", null, ["number"], [bufferPtr]);
+        return;
     }
-}
+
+    // If we make it this far, it instead means that we are calling the render in C++
+    const { yaml, maxDepth, renderId } = e.data;
+    activeRenderId = renderId;
+
+    // Initialize WASM module once
+    if (!Module) {
+        Module = await initWasm();
+    }
+
+    // Calling the c++ render function
+    Module.ccall("render", null, ["string", "number"], [yaml, maxDepth]);
+};
